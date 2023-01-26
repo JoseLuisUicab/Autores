be0ebd6c83b92377a0f1f0528386b87f5a32cc5f
JoseLuisuicab <joseluisuicabcan30@gmail.com>
2023-01-26 10:13:34 -0600

    assass

--- Scrip/Scrip.js
+++ Scrip/Scrip.js
@@ -31,7 +31,6 @@ function Cargarexcel() {
 
     }
 
-
-
   })
+  return false;
 }
---
+++ src/importa.php
@@ -0,0 +1,12 @@
+<?php
+//importar la ruta del phpexcel
+require "excel.php";
+
+$archivos = "maiaa.xlsx";
+
+// cargar nuestra hoja de excel
+$excel = PHPExcel_IOFactory::load($archivos);
+ // cargar lla hoja de calculos 
+$excel->setActiveSheetIndex(0);
+$numfila->
+?>
