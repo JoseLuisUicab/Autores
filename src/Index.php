05b2bd9732da6e9c3bb4c90ab58ae532b3d0e861
William_Gongora <wgongorabojorquez@gmail.com>
2023-01-29 13:28:52 -0600

    Cambios 29/01/2023
    Avances

--- src/Index.php
+++ src/Index.php
@@ -6,15 +6,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="src/style.css">
   <link rel="stylesheet" href="../Css/Destock.css">
-  <script src="../Scrip/Scrip2.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+  <script src="../Scrip/Scrip2.js"></script>
   <script src='https://kit.fontawesome.com/2ee0245f3d.js' crossorigin='anonymous'></script>
   <title>Principal</title>
 </head>
 <body>
 
   <?php include "menu.php"?>
-  <Section class="Inicio"></Section>
+  <Section class="Inicio">
+    <h2>Nuestros Colaboradores</h2>
+  </Section>
   <h2 class="teamle">TEAM LEADERS</h2>
 
   <Section class="Parte1">
@@ -24,98 +26,44 @@
   <h3>Senior Front-End</h3>
   <br>
   <p>ING.William Gongora Bojorquez</p>
-  <div class="iconos">
-  <i class="fa-brands fa-facebook"></i>
-  <i class="fab fa-linkedin-in"></i>
-  <i class="fas fa-envelope"></i>
-  </div>
   <div class="aportaciones">
     <h3>Aportaciones</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, necessitatibus?dasdasfd
       Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      <div class="iconos">
+    <i class="fa-brands fa-facebook"></i>
+    <i class="fab fa-linkedin-in"></i>
+    <i class="fas fa-envelope"></i>
+    </div>
     </p>
   </div>
   </div>
-  
-
-  <div class="persona">
-  <figure><img src="../Imagenes/Billgates.jpg" alt=""></figure>
-  <h3>Senior Front-End</h3>
-  <br>
-  <p>ING.William Gongora Bojorquez</p>
-  <div class="iconos">
-  <i class="fa-brands fa-facebook"></i>
-  <i class="fab fa-linkedin-in"></i>
-  <i class="fas fa-envelope"></i>
-  </div>
-  <div class="aportaciones">
-    <h3>Aportaciones</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, necessitatibus?dasdasfd
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    </p>
-  </div>
-  </div>
-
-  <div class="persona">
-  <figure><img src="../Imagenes/Billgates.jpg" alt=""></figure>
-  <h3>Senior Front-End</h3>
-  <br>
-  <p>ING.William Gongora Bojorquez</p>
-  <div class="iconos">
-  <i class="fa-brands fa-facebook"></i>
-  <i class="fab fa-linkedin-in"></i>
-  <i class="fas fa-envelope"></i>
-  </div>
-  <div class="aportaciones">
-    <h3>Aportaciones</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, necessitatibus?dasdasfd
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    </p>
-  </div>
-  </div>
-
-
-  
-  <div class="persona">
-  <figure><img src="../Imagenes/Billgates.jpg" alt=""></figure>
-  <h3>Senior Front-End</h3>
-  <br>
-  <p>ING.William Gongora Bojorquez</p>
-  <div class="iconos">
-  <i class="fa-brands fa-facebook"></i>
-  <i class="fab fa-linkedin-in"></i>
-  <i class="fas fa-envelope"></i>
-  </div>
-  <div class="aportaciones">
-    <h3>Aportaciones</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, necessitatibus?dasdasfd
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    </p>
-  </div>
-  </div>
-
-  <div class="persona">
-  <figure><img src="../Imagenes/Billgates.jpg" alt=""></figure>
-  <h3>Senior Front-End</h3>
-  <br>
-  <p>ING.William Gongora Bojorquez</p>
-  <div class="iconos">
-  <i class="fa-brands fa-facebook"></i>
-  <i class="fab fa-linkedin-in"></i>
-  <i class="fas fa-envelope"></i>
-  </div>
-  <div class="aportaciones">
-    <h3>Aportaciones</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, necessitatibus?dasdasfd
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    </p>
-  </div>
-  </div>
-  
   </article>
 
 </Section>
 <h2 class="teamle">Desarrolladores</h2>
+<Section class="Parte2">
+  <article class="leader2">
+  <div class="persona2">
+  <figure><img src="../Imagenes/Billgates.jpg" alt=""></figure>
+  <h3>Senior Front-End</h3>
+  <br>
+  <p>ING.William Gongora Bojorquez</p>
+  <div class="aportaciones2">
+    <h3>Aportaciones</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, necessitatibus?dasdasfd
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      <div class="iconos2">
+    <i class="fa-brands fa-facebook"></i>
+    <i class="fab fa-linkedin-in"></i>
+    <i class="fas fa-envelope"></i>
+    </div>
+    </p>
+  </div>
+  </div>
+  </article>
+
+</Section>
 <?php include "Footer.php"?>
 </body>
 
--- src/Login.php
+++ src/Login.php
@@ -4,9 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/style.css">
+  <link rel="stylesheet" href="../Css/Destock.css">
+  <script src="../Scrip/Scrip2.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+  <script src='https://kit.fontawesome.com/2ee0245f3d.js' crossorigin='anonymous'></script>
     <title>Document</title>
 </head>
 <body>
-    <h2>Albert</h2>
+    <section class="Login">
+        <figure><img src="../Imagenes/maia.png" alt=""></figure>
+        <div>
+        <form class="Formulario" action="">
+            <p>Administrador</p><br>
+            <label for="">Usuario:</label>
+            <input type="text"><br>
+            <label for="">Contraseña</label>
+            <input type="text"><br>
+            <button>Iniciar</button><br>
+            <button>Regresar</button>
+
+        </form>
+        </div>
+    </section>
 </body>
 </html>
