011b92e09eee5b35a6ef7d77856c8f222fca8023
William_Gongora <wgongorabojorquez@gmail.com>
2023-01-30 10:04:24 -0600

    Cambios 30/01/2023
    Cabios en validacion login

---
+++ src/controller.php
@@ -0,0 +1,19 @@
+
+<?php
+ /*Verificacionde boton */
+ if(!empty($_POST["btniniciar"])){
+        if (empty($_POST["usuario"]) and empty ($_POST["contrasena"])) {
+            echo '<script language="javascript">alert("Porfavor de llenar los Datos");</script>';
+        } else {
+        $usuario=$_POST["usuario"];
+        $contrasenia=$_POST["contrasena"];
+        $sql=$conexion->query(" select * from login where Nombre='$usuario' and Contraseña='$contrasenia' ");
+        if ($datos=$sql->fetch_object()) {
+            header("Location:Index.php");
+        } else {
+            echo'<script language="javascript">alert("Los datos son Incorrectos");</script>';
+        }
+        
+        }
+ }
+?>
