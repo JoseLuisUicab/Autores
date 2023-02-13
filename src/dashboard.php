062e2fd8e0247376a15f0b58d27500cf63a24966
JoseLuisuicab <joseluisuicabcan30@gmail.com>
2023-02-13 09:10:04 -0600

    ww

--- src/dashboard.php
+++ src/dashboard.php
@@ -54,7 +54,7 @@
     <br><br>
     <div class="text-center container-fluid">
       <div class="">
-        <button class="btn btn-primary"></button>
+        <button class="btn btn-primary boton_borrar">Borrar todo los Registros</button>
       </div>
       <h1 class=" fw-bold fs-2 p-3">Lista de Integrantes</h1>
       <table class="table table-striped table-bordered table-responsive  " id="table_admin">
