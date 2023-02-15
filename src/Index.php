9253c403bc4112034dedd146b5944a678c4bf849
William_Gongora <wgongorabojorquez@gmail.com>
2023-02-15 09:02:04 -0600

    Listo

--- src/Index.php
+++ src/Index.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../Css/Destock.css">
-
+  <link rel="stylesheet" href="../Css/Movil.css">
   <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
 
   <script src='https://kit.fontawesome.com/2ee0245f3d.js' crossorigin='anonymous'></script>
